fdc2595c21eec5ae16faf0ce76ab4175871f5061
Pushkar Singh <stoazest@gmail.com>
2022-11-15 12:11:36 +0530

    socket.io configuration

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const io = new Server(server, {
 		methods : ["GET", "POST"],
 		credentials : true,
 		transports : [`websocket`, `polling`]
-}
+}, allowEIO3 : true,
 });
 
 // variable to increment messageId.
